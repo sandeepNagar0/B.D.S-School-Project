e2638d3a6033781cc9f93553b9699ceb9fcae01a
sandeep Nagar <uffyefacts@gmail.com>
2026-04-27 15:53:55 +0530

    add and changes

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 let scrollbtn=document.querySelector('#scrollbtn')
 
-scrollbtn.addEventListener('click', ()=>{
+scrollbtn.addEventListener('click', () => {
    window.scrollTo({
       top:document.body.scrollHeight,
       behavior:"smooth"
    })
+})
+
+
+let hemburgerIconBtn=document.querySelector('.humberger-icon')
+let closeBtn=document.querySelector('.close-icon')
+let moblileNavbar=document.querySelector('.mobile-navbar')
+
+hemburgerIconBtn.addEventListener('click', ()=>{
+    moblileNavbar.style.left='0px'
+})
+
+closeBtn.addEventListener("click", () => {
+   moblileNavbar.style.left='-500px'
 })
